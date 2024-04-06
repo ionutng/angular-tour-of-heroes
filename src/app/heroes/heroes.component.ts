f346039bb9b39828ef6d147b4b81f27ccd13cc5f
ionutng <ionutngp@gmail.com>
2024-04-06 10:54:25 +0300

    Add hero service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { HEROES } from '../mock-heroes';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,9 +9,20 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent {
   selectedHero?: Hero;
-  heroes = HEROES;
+  heroes: Hero[] = [];
   
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  constructor(private heroService: HeroService) { }
 }
